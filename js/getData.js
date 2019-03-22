0dc5014e9c05ea3e418947758c1153188e1e8289
varnerstanley <stanmanjr28@gmail.com>
2019-03-22 13:37:29 -0600

    to github

--- js/getData.js
+++ js/getData.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  const Url = 'http://localhost:3000/gPosts';
-  $.ajax({
-    url: Url,
-    type: "GET",
-    success: function(result){
-      console.log(result)
-    },
-    error: function(error){
-      console.log('Error ${error}')
-    }
-  })
-})
+// $(document).ready(function(){
+//   const Url = 'http://localhost:3000/gPosts';
+//   $.ajax({
+//     url: Url,
+//     type: "GET",
+//     success: function(result){
+//       console.log(result)
+//     },
+//     error: function(error){
+//       console.log('Error ${error}')
+//     }
+//   })
+// })
